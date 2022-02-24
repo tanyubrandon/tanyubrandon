fc2722d8624516099d4cf3e9dba2752d9a544607
tanyubrandon <tanyubrandon876@gemail.com>
2022-02-24 15:21:19 +0100

    deploy react app to github pages

---
+++ src/Create.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    })
+    .then(() => {
+      // history.go(-1);
+      history.push('/');
+    })
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="Tanyu">Tanyu</option>
+          <option value="Brandon">Brandon</option>
+          <option value="Andy">Andy</option>
+          <option value="Cecilia">Cecilia</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+}
+ 
+export default Create;
